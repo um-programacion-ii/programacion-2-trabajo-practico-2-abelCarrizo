6c52c930e3f32c43126f7df5b080b8ebc69fd5ca
abelCarrizo <aa.carrizo@alumno.um.edu.ar>
2025-04-23 00:47:55 +0000

    feat: añadir recursos concretos

---
+++ Sistema-de-Gestion-de-Biblioteca-Digital/src/main/java/app/biblioteca/recursos/AudioLibro.java
@@ -0,0 +1,31 @@
+package app.biblioteca.recursos;
+
+import app.biblioteca.utils.CategoriaRecurso;
+import app.biblioteca.utils.EstadoRecurso;
+
+public class AudioLibro extends RecursoDigital {
+    private int duracionHoras;
+
+    public AudioLibro(String titulo, String autor, EstadoRecurso estado, CategoriaRecurso categoria, int anioPublicacion, int duracionHoras) {
+        super(titulo, autor, estado, categoria, anioPublicacion);
+        this.duracionHoras = duracionHoras;
+    }
+
+    public int getDuracionHoras() {
+        return duracionHoras;
+    }
+
+    public void setDuracionHoras(int duracionHoras) {
+        if (duracionHoras <= 0) {
+            throw new IllegalArgumentException("El número de horas debe ser mayor que cero.");
+        }
+        this.duracionHoras = duracionHoras;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("AudioLibro: " + titulo + " por " + autor + " [" + estado + "]");
+    }
+}
+
+
---
+++ Sistema-de-Gestion-de-Biblioteca-Digital/src/main/java/app/biblioteca/recursos/Libro.java
@@ -0,0 +1,30 @@
+package app.biblioteca.recursos;
+
+import app.biblioteca.utils.CategoriaRecurso;
+import app.biblioteca.utils.EstadoRecurso;
+
+public class Libro extends RecursoDigital {
+    private int numeroPaginas;
+
+    public Libro(String titulo, String autor, EstadoRecurso estado, CategoriaRecurso categoria, int anioPublicacion, int numeroPaginas) {
+        super(titulo, autor, estado, categoria, anioPublicacion);
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        if (numeroPaginas <= 0) {
+            throw new IllegalArgumentException("El número de páginas debe ser mayor que cero.");
+        }
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Libro: " + titulo + " por " + autor + " [" + estado + "]");
+    }
+}
+
